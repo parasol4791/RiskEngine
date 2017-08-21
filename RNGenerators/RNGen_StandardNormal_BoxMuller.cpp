f58ef7be1288be30d59c2c82f9275fbbd12f1ce1
parasol4791 <dvk11@gmx.com>
2017-08-20 21:14:32 -0400

    Added RiskEndingRoot as additional include directory

--- RNGenerators/RNGen_StandardNormal_BoxMuller.cpp
+++ RNGenerators/RNGen_StandardNormal_BoxMuller.cpp
@@ -1,6 +1,6 @@
 #include "RNGen_StandardNormal_BoxMuller.h"
 #include "RNGen_Uniform.h"
-//#include "Maths/MathFunctions.h"
+#include "Maths/MathFunctions.h"
 
 
 RNGen_StandardNormal_BoxMuller::RNGen_StandardNormal_BoxMuller()
@@ -17,7 +17,7 @@ double RNGen_StandardNormal_BoxMuller::getRN() const
 	double u1 = uniformGen.getRN();
 	double u2 = uniformGen.getRN();
 
-	//double pi = MathFunctions::pi();
+	double pi = MathFunctions::pi();
 	//double first = sqrt(2*)
 	double z1(0);
 	return z1;
